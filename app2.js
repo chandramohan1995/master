b30e626575f20f0055985ec7230e9d3c1af951be
chandramohan <g.chandramohan1995@gmail.com>
2018-03-14 17:23:28 +0530

    added

---
+++ app2.js
@@ -0,0 +1,33 @@
+console.log('running');
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+
+var stream = T.stream('user');
+ 
+
+stream.on('follow',function(userfollowed){
+   var name = userfollowed.source.name;
+   var id = userfollowed.source.screen_name;
+    tweetit('.@'+name+' thanks for following,,see ya ');
+});
+
+
+
+function tweetit(txt){
+    var r = Math.floor(Math.random()*100);
+  var  tweet={
+       status: txt +' '+ r
+     }
+
+ T.post('statuses/update',tweet, function(err, data, response) {
+     if(err){
+         console.log(err);
+     }else{
+         
+    console.log('it worked');
+  }
+ });
+
+
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'HPXbnUqXzsYLcwRpm0DTC4hIl',
+  consumer_secret:      '4CN4cIK9dDKSAJuAeRm2hF5KMfeXYuxlktYXZHIbylXJbDn1IT',
+  access_token:         '973152271583232000-Z6Pv6FAZ3N5OIN08e9MxhffV61t33lJ',
+  access_token_secret:  'sIuXTNn36i2mNU84jGqNL4lQyq6fgCnh24wKR985XInbI',
+}
